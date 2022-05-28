06a5da66e629c5cce81469ae3f79b270d8d29724
Adam Demjen <demjened@gmail.com>
2022-05-28 17:50:49 -0400

    Add tests

---
+++ src/test/java/io/demjened/riff/model/RiffDataTest.java
@@ -0,0 +1,34 @@
+package io.demjened.riff.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RiffDataTest {
+
+    @Test
+    public void testConstructor() {
+        RiffData<Object> subject = new RiffData<>();
+
+        assertEquals(Map.of(
+                ChangeType.ADDED, Set.of(),
+                ChangeType.REMOVED, Set.of(),
+                ChangeType.MODIFIED, Set.of(),
+                ChangeType.UNMODIFIED, Set.of()), subject.getChanges());
+    }
+
+    @Test
+    public void testToString() {
+        RiffData<Object> subject = new RiffData<>();
+
+        String subjectAsString = subject.toString();
+
+        assertTrue(subjectAsString.contains("left="));
+        assertTrue(subjectAsString.contains("right="));
+        assertTrue(subjectAsString.contains("changes="));
+    }
+
+}
