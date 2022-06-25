416827e8cc6a0077d1f4746b35717b05eecb96fb
Adam Demjen <demjened@gmail.com>
2022-06-25 16:20:40 -0400

    Add docs

--- src/main/java/io/demjened/riff/generators/AbstractRiffGenerator.java
+++ src/main/java/io/demjened/riff/generators/AbstractRiffGenerator.java
@@ -24,6 +24,13 @@ public abstract class AbstractRiffGenerator<T> implements RiffGenerator<T> {
         return data;
     }
 
+    /**
+     * Sets the left collection. Replaces the previous one if there's one.<br/>
+     * Subclasses should override this method and downcast the return type.
+     *
+     * @param left Left collection
+     * @return The generator object
+     */
     protected AbstractRiffGenerator<T> setLeft(Collection<T> left) {
         data.getLeft().clear();
         data.getLeft().addAll(left);
@@ -31,6 +38,13 @@ public abstract class AbstractRiffGenerator<T> implements RiffGenerator<T> {
         return this;
     }
 
+    /**
+     * Sets the right collection. Replaces the previous one if there's one.<br>
+     * Subclasses should override this method and downcast the return type.
+     *
+     * @param right Right collection
+     * @return The generator object
+     */
     protected AbstractRiffGenerator<T> setRight(Collection<T> right) {
         data.getRight().clear();
         data.getRight().addAll(right);
@@ -38,18 +52,38 @@ public abstract class AbstractRiffGenerator<T> implements RiffGenerator<T> {
         return this;
     }
 
+    /**
+     * Marks the item as added.
+     *
+     * @param item The item
+     */
     protected void added(T item) {
         changed(item, ChangeType.ADDED);
     }
 
+    /**
+     * Marks the item as removed.
+     *
+     * @param item The item
+     */
     protected void removed(T item) {
         changed(item, ChangeType.REMOVED);
     }
 
+    /**
+     * Marks the item as modified.
+     *
+     * @param item The item
+     */
     protected void modified(T item) {
         changed(item, ChangeType.MODIFIED);
     }
 
+    /**
+     * Marks the item as unmodified.
+     *
+     * @param item The item
+     */
     protected void unmodified(T item) {
         changed(item, ChangeType.UNMODIFIED);
     }
--- src/main/java/io/demjened/riff/generators/RiffGenerator.java
+++ src/main/java/io/demjened/riff/generators/RiffGenerator.java
@@ -3,12 +3,19 @@ package io.demjened.riff.generators;
 import io.demjened.riff.model.RiffData;
 
 /**
- * Riff generator.
+ * Riff generator.<br/>
+ * Implementing classes should follow the builder pattern where the last method call in the chain is
+ * {@link #generate()} to generate the riff.
  *
  * @param <T> Type of items to diff
  */
 public interface RiffGenerator<T> {
 
+    /**
+     * Generates the riff data.
+     *
+     * @return Generated riff data
+     */
     RiffData<T> generate();
 
 }
